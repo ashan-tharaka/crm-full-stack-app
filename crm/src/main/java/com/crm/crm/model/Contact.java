1141524e6384ac0c0bf7a3b6e3a68c8d2c9b8175
ashan-tharaka <ashantharaka715@gmail.com>
2022-06-14 20:16:06 +0530

    Initial commit

---
+++ crm/src/main/java/com/crm/crm/model/Contact.java
@@ -0,0 +1,29 @@
+package com.crm.crm.model;
+
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Data
+@Entity
+
+public class Contact {
+  public @Id @GeneratedValue long id;
+  public String firstName;
+  public String lastName;
+  public String email;
+  public Contact(){
+
+  }
+
+  public Contact(String firstName, String lastName, String email) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.email = email;
+  }
+
+
+}
